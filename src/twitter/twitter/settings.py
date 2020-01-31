55e52b3456717f577998029f7915a3ccc1728d89
Jakub Vymola <kuba.vymola@gmail.com>
2020-01-31 20:04:54 +0200

    Working hello world template

--- src/twitter/twitter/settings.py
+++ src/twitter/twitter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'twitter_main.apps.TwitterMainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'twitter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
